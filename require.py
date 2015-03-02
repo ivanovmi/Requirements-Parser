903a35e1d7ef0235b968f3a3bb0ce36ffc11f65c
dtrishkin <dtrishkin@mirantis.com>
2015-03-02 12:25:08 +0300

    Fix checking with '=='

--- require.py
+++ require.py
@@ -1,3 +1,6 @@
+import itertools
+import sys
+
 class Require:
 
 	packs = dict()
@@ -15,34 +18,57 @@ class Require:
 			req[el] = set(req[el]) | set(req1[el])
 			req[el] = sorted(req[el], key = lambda x: x[1])
 
-			pred = None
-			idx = 0
-			while idx < len(req[el]):
-				if pred != None:
-					if req[el][idx][1] == pred[1]:
-						if req[el][idx][0] == '!=':
-							req[el][idx], req[el][idx - 1] = req[el][idx - 1], req[el][idx]
-						if req[el][idx][0] in ['>', '<']:
-							req[el].pop(idx - 1)
-						elif req[el][idx][0] in ['>=', '<=']:
-							if req[el][idx - 1][0] != '!=':
-								req[el].pop(idx)
-							else:
-								req[el][idx] = (req[el][idx][0][:1], req[el][idx][1])
-								req[el].pop(idx - 1)
-						idx -= 1
-				pred = req[el][idx]
-				idx += 1
+			signs = [x[0] for x in reversed(req[el])]
 
-			res = filter(lambda x: x[0] == '!=', req[el])
-			for i in reversed(req[el]):
-				if i[0] in ['>', '>=']:
-					res.append(i)
-					break
-			for i in req[el]:
-				if i[0] in ['<', '<=']:
-					res.append(i)
-					break
-			req[el] = res
+			try:
+				sI = signs.index('==')
+				eqEl = True
+			except ValueError:
+				sI = sys.maxint
+				eqEl = False
+
+			if eqEl:
+				eqEl = itertools.ifilter(lambda x: x[0] == '==', req[el])
+				for sign in ['<=', '>=', '<', '>']:
+					try:
+						fI = signs.index(sign)
+					except ValueError:
+						fI = sys.maxint
+					if fI <= sI:
+						eqEl = False
+						break;
+
+			if not eqEl:
+				pred = None
+				idx = 0
+				while idx < len(req[el]):
+					if pred != None:
+						if req[el][idx][1] == pred[1]:
+							if req[el][idx][0] in ['!=', '==']:
+								req[el][idx], req[el][idx - 1] = req[el][idx - 1], req[el][idx]
+							if req[el][idx][0] in ['>', '<']:
+								req[el].pop(idx - 1)
+							elif req[el][idx][0] in ['>=', '<=']:
+								if req[el][idx - 1][0] != '!=':
+									req[el].pop(idx)
+								else:
+									req[el][idx] = (req[el][idx][0][:1], req[el][idx][1])
+									req[el].pop(idx - 1)
+							idx -= 1
+					pred = req[el][idx]
+					idx += 1
+
+				res = filter(lambda x: x[0] == '!=', req[el])
+				for i in reversed(req[el]):
+					if i[0] in ['>', '>=']:
+						res.append(i)
+						break
+				for i in req[el]:
+					if i[0] in ['<', '<=']:
+						res.append(i)
+						break
+				req[el] = res
+			else:
+				req[el] = list(eqEl)
 
 		return req
