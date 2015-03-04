a4a3cae8da156f1d7163045e8303b0998dfbc5c8
Michael Ivanov <mivanov@michael-aspire-5733z.Dlink>
2015-03-04 23:44:03 +0300

    Initial commit. Need to change output of main file.

--- reqParse.py
+++ reqParse.py
@@ -4,21 +4,22 @@ packageName = re.compile("[a-zA-Z0-9-_.]+")
 packageEq = re.compile("(>=|<=|>|<|==|!=)+")
 packageVers = re.compile("[\d.]+")
 
+
 def parseReq(inp):
-	res = dict()
-	#with open(input, 'r') as f:
-	for line in inp:
-		if line[0] == '#':
-			continue
-		resName = packageName.search(line)
-		resEq = packageEq.findall(line)
-		it = next(re.finditer('>=|<=|>|<|==|!=', line), None)
-		if it:
-			resVers = packageVers.findall(line[it.start():])
-		if resName:
-			name = resName.group(0)
-			if not res.has_key(name):
-				res[name] = set()
-				for idx, sign in enumerate(resEq):
-					res[name].add((sign, resVers[idx]))
-	return res
+    res = dict()
+    #with open(input, 'r') as f:
+    for line in inp:
+        if line == '' or line[0] == '#':
+            continue
+        resName = packageName.search(line)
+        resEq = packageEq.findall(line)
+        it = next(re.finditer('>=|<=|>|<|==|!=', line), None)
+        if it:
+            resVers = packageVers.findall(line[it.start():])
+        if resName:
+            name = resName.group(0)
+            if not res.has_key(name):
+                res[name] = set()
+                for idx, sign in enumerate(resEq):
+                    res[name].add((sign, resVers[idx]))
+    return res
