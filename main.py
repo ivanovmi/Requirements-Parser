bf3d3406b2b0a32f2a660aa3339d15e3a366bdd2
Michael Ivanov <mivanov@michael-aspire-5733z.Dlink>
2015-03-05 00:04:02 +0300

    Change output style. TODO: convert stdout to json-files

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     with open('req', 'r') as req_file:
         for repo in req_file:
             print "Repos:", repo
-            req_url = 'https://review.fuel-infra.org/gitweb?p=openstack/horizon.git;a=blob_plain;f=requirements.txt;hb=refs/heads/master'
+            req_url = 'https://review.fuel-infra.org/gitweb?p=openstack/' + repo.strip() + '.git;a=blob_plain;f=requirements.txt;hb=refs/heads/master'
             r = lan.getRequirementsFromUrl(req_url, gerritAccount)
             rq1 = require.Require(reqParse.parseReq(r))
             rq = require.Require.merge(rq1.packs, rq2.packs)
-            #for i in rq.items():
-            #    print i
+            for key in rq1.packs.keys():
+                print "{0} {1}".format(key, rq[key])
     #os.system("./emailSend.sh {0} "'"{1}"'"".format("asteroid566@gmail.com", rq.items()))
     #for i in rq1.packs.items():
     #	print i
-            for key in rq1.packs.keys():
-                print "{0} {1}".format(key, rq[key])
+    #for key in rq1.packs.keys():
+    #	print "{0} {1}".format(key, rq[key])
