8641242da880d12d0fbe534bf5d6930f74da5866
Michael Ivanov <mivanov@michael-aspire-5733z.Dlink>
2015-03-05 00:09:32 +0300

    Change output style. TODO: convert stdout to json file

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     with open('req', 'r') as req_file:
         for repo in req_file:
             print "Repos:", repo
-<<<<<<< HEAD
             req_url = 'https://review.fuel-infra.org/gitweb?p=openstack/' + repo.strip() + '.git;a=blob_plain;f=requirements.txt;hb=refs/heads/master'
             r = lan.getRequirementsFromUrl(req_url, gerritAccount)
             rq1 = require.Require(reqParse.parseReq(r))
@@ -24,16 +23,3 @@ if __name__ == "__main__":
     #	print i
     #for key in rq1.packs.keys():
     #	print "{0} {1}".format(key, rq[key])
-=======
-            req_url = 'https://review.fuel-infra.org/gitweb?p=openstack/horizon.git;a=blob_plain;f=requirements.txt;hb=refs/heads/master'
-            r = lan.getRequirementsFromUrl(req_url, gerritAccount)
-            rq1 = require.Require(reqParse.parseReq(r))
-            rq = require.Require.merge(rq1.packs, rq2.packs)
-            #for i in rq.items():
-            #    print i
-    #os.system("./emailSend.sh {0} "'"{1}"'"".format("asteroid566@gmail.com", rq.items()))
-    #for i in rq1.packs.items():
-    #	print i
-            for key in rq1.packs.keys():
-                print "{0} {1}".format(key, rq[key])
->>>>>>> 79f0eacf59eff31a3264ce37d0e00374117f84a2
