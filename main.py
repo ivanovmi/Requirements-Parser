112accb388553a1ee46ab3cd13ec9b98d6db5dc4
Michael Ivanov <mivanov@michael-aspire-5733z.Dlink>
2015-03-05 00:09:32 +0300

    Added check of branch name. TODO: convert stdout to json file

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import os
 import lan
 
 if __name__ == "__main__":
+    branch_name = raw_input("At the what branch we should check requirements? ")
+    if branch_name == 'master':
+        branch = 'master'
+    elif branch_name == '6.1':
+        branch = 'openstack-ci/fuel-6.1/2014.2'
+    elif branch_name == '6.0.1':
+        branch = 'openstack-ci/fuel-6.0.1/2014.2'
     #pdb.set_trace()
     gerritAccount = lan.loginToLaunchpad()
     with open("2ndReq", 'r') as f:
@@ -12,8 +19,7 @@ if __name__ == "__main__":
     with open('req', 'r') as req_file:
         for repo in req_file:
             print "Repos:", repo
-<<<<<<< HEAD
-            req_url = 'https://review.fuel-infra.org/gitweb?p=openstack/' + repo.strip() + '.git;a=blob_plain;f=requirements.txt;hb=refs/heads/master'
+            req_url = 'https://review.fuel-infra.org/gitweb?p=openstack/' + repo.strip() + '.git;a=blob_plain;f=requirements.txt;hb=refs/heads/' + branch
             r = lan.getRequirementsFromUrl(req_url, gerritAccount)
             rq1 = require.Require(reqParse.parseReq(r))
             rq = require.Require.merge(rq1.packs, rq2.packs)
@@ -24,16 +30,3 @@ if __name__ == "__main__":
     #	print i
     #for key in rq1.packs.keys():
     #	print "{0} {1}".format(key, rq[key])
-=======
-            req_url = 'https://review.fuel-infra.org/gitweb?p=openstack/horizon.git;a=blob_plain;f=requirements.txt;hb=refs/heads/master'
-            r = lan.getRequirementsFromUrl(req_url, gerritAccount)
-            rq1 = require.Require(reqParse.parseReq(r))
-            rq = require.Require.merge(rq1.packs, rq2.packs)
-            #for i in rq.items():
-            #    print i
-    #os.system("./emailSend.sh {0} "'"{1}"'"".format("asteroid566@gmail.com", rq.items()))
-    #for i in rq1.packs.items():
-    #	print i
-            for key in rq1.packs.keys():
-                print "{0} {1}".format(key, rq[key])
->>>>>>> 79f0eacf59eff31a3264ce37d0e00374117f84a2
