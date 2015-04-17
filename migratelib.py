03f96431b7258f78221f9b7f01094364038cd405
Michael Ivanov <difmel@gmail.com>
2015-04-17 17:20:59 +0300

    Update new module
    Add more informative output

--- migratelib.py
+++ migratelib.py
@@ -6,52 +6,73 @@ import require_utils
 import os
 import json
 
-version = re.compile("^[0-9_.:]+")
+version_mask = re.compile("^[0-9_.:]+")
 
 os.system("aptitude -F '%p %V' --disable-columns search '~n . ~O Mirantis' >> tmp")
 
 
-def versiontuple(v):
+def compare_version(v):
     return tuple(map(int, (v.split("."))))
 
 
 def compare(version, sets):
+    status = []
     a = list(sets)
-
+    a.sort(reverse=True)
     if len(a)>0:
         for i in a:
             if i[0] == '<':
-                print versiontuple(version) < versiontuple(i[1])
+                if compare_version(version) < compare_version(i[1]):
+                    status.append(True)
+                else:
+                    status.append(False)
             elif i[0] == '>':
-                print versiontuple(version) > versiontuple(i[1])
+                if compare_version(version) > compare_version(i[1]):
+                    status.append(True)
+                else:
+                    status.append(False)
             elif i[0] == '==':
-                print versiontuple(version) == versiontuple(i[1])
+                if compare_version(version) == compare_version(i[1]):
+                    status.append(True)
+                else:
+                    status.append(False)
             elif i[0] == '>=':
-                print versiontuple(version) >= versiontuple(i[1])
+                if compare_version(version) >= compare_version(i[1]):
+                    status.append(True)
+                else:
+                    status.append(False)
             elif i[0] == '<=':
-                print versiontuple(version) <= versiontuple(i[1])
+                if compare_version(version) <= compare_version(i[1]):
+                    status.append(True)
+                else:
+                    status.append(False)
             elif i[0] == '!=':
-                print versiontuple(version) != versiontuple(i[1])
+                if compare_version(version) != compare_version(i[1]):
+                    status.append(True)
+                else:
+                    status.append(False)
+    if False in status:
+        print "The dependencie wrong on " + str(status.index(False)+1) + " border"
     else:
-        print 'asd'
+        print 'All OK'
 
 packages_dict = {}
+
 with open("tmp", "r") as f:
     for line in f:
         package_name = line.split(" ")[0]
-        pre_vers = line.split(" ")[1].strip()
-        package_vers = version.search(pre_vers)
-        if package_vers:
-            vers = package_vers.group(0)
+        apt_version = line.split(" ")[1].strip()
+        version = version_mask.search(apt_version)
+        if version:
+            package_version = version.group(0)
         else:
-            vers = None
+            package_version = None
         try:
-            if ':' in vers:
-                vers = vers.split(':')[1]
+            if ':' in package_version:
+                package_version = package_version.split(':')[1]
         except TypeError:
             pass
-
-        packages_dict[package_name] = vers
+        packages_dict[package_name] = package_version
 
 try:
     os.remove('tmp')
@@ -60,13 +81,13 @@ except OSError:
 
 
 # Get global requirements
-gerritAccount=lan.login_to_launchpad("mivanov@mirantis.com", "16121814")
+gerritAccount=lan.login_to_launchpad()
 req_url = 'https://raw.githubusercontent.com/openstack/requirements/' \
             '{0}/global-requirements.txt'.format("stable/kilo")
 r = lan.get_requirements_from_url(req_url, gerritAccount)
 rq2 = require_utils.Require.parse_req(r)
 
-
+# change names according base-control rules
 control_base_file = open("control-base.json", "r")
 control_base = json.load(control_base_file)
 for item in rq2:
@@ -74,11 +95,11 @@ for item in rq2:
         if item == basename:
             rq2[key]=rq2.pop(item)
 
+# generate cli output
 for key in packages_dict:
     if key in rq2:
         print key
-        try:
-            print packages_dict[key] + "    ========    " + str(list(rq2[key]))
-            compare(packages_dict[key], rq2[key])
-        except TypeError:
-            print "None" + "    ========    " + str(list(rq2[key]))
+        a = list(rq2[key])
+        a.sort(reverse=True)
+        print packages_dict[key] + "    ========    " + str(a)
+        compare(packages_dict[key], rq2[key])
